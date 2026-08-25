ecdc4d8516f0fa4518bf4c164c41e5a099685a69
auditdvs <dvsaudit@gmail.com>
2026-08-25 14:10:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -775,13 +775,11 @@ def build_excel(alloc: pd.DataFrame, harga_beras, harga_galon, harga_isi_ulang) 
 # =========================================================
 
 st.title("Breakdown Biaya Konsumsi Bulanan")
-st.write(
-    "- Upload laporan Transaction Listing By Accounts (file original **.xls nya langsung ya tanpa di edit apapun) untuk akun Biaya Konsumsi. "
-    "- Aplikasi ini memecah transaksi per bulan menjadi: kopi/gula/teh, beras, galon "
-    "- aqua asli (beli baru), isi ulang galon, serta jumsih dan mini training."
-)
+st.write("- Upload laporan Transaction Listing By Accounts (file original **.xls nya langsung ya tanpa di edit apapun) untuk akun Biaya Konsumsi. ")
+st.write("- Aplikasi ini memecah transaksi per bulan menjadi: kopi/gula/teh, beras, galon ")
+st.write("- aqua asli (beli baru), isi ulang galon, serta jumsih dan mini training.")
 
-uploaded = st.file_uploader("Upload file laporan (.xls, .xlsx, atau .csv)", type=["xls", "xlsx", "csv"])
+uploaded = st.file_uploader("Upload file laporan (.xls)", type=["xls"])
 
 if uploaded is None:
     st.info("Silakan upload file laporan konsumsi untuk mulai.")
